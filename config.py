a5a00050cdf51940a56c93f815e5951b6951c6cb
slavikyd <47947352+slavikyd@users.noreply.github.com>
2022-11-20 11:39:55 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,1 @@
-#token = 'MTAzOTYwOTAzMTc2NzEwMTQ3MA.GpiCVd.cndmBEh_9y2xviuKiTJKcG--mD4YhOUxBirhkQ' discord token :(
-
-
-
-
-token1 = '5741488433:AAFTeiso3aZ-7QtizQqLZVgIJrydjJfBSRs'
+token1 = 'токен'
